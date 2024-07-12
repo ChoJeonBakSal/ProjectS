fbae550ca19eeaeaca5a30922178d9a5eeb90af8
JinMyung <kjm990617@gmail.com>
2024-07-12 13:37:00 +0900

    [Update] Player와 Player_Sub의 Z축 위치에 따라 Renderer Camera Stack 순서 변경되는 로직 추가

---
+++ Assets/Scripts/Camera/CameraObejctSortManager.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class CameraObejctSortManager : MonoBehaviour
+{
+    [SerializeField] private GameObject Player;
+    [SerializeField] private GameObject Player_Sub;
+
+    private Camera camera;
+    [SerializeField] private Camera Player_Camera;
+    [SerializeField] private Camera Sub_Camera;
+
+
+    private void Start()
+    {
+        camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        UpdateCameraStackOrder();
+    }
+
+    public void ChangeCameraStackOrder(int oldIndex, int newIndex)
+    {
+        var baseCameraData = camera.GetUniversalAdditionalCameraData();
+        if (baseCameraData != null && oldIndex < baseCameraData.cameraStack.Count && newIndex < baseCameraData.cameraStack.Count)
+        {
+            var cameraToMove = baseCameraData.cameraStack[oldIndex];
+            baseCameraData.cameraStack.RemoveAt(oldIndex);
+            baseCameraData.cameraStack.Insert(newIndex, cameraToMove);
+        }
+        else
+        {
+            Debug.LogError("Invalid indices for camera stack.");
+        }
+    }
+
+    private void UpdateCameraStackOrder()
+    {
+        float zPositionPlayer = Player.transform.position.z;
+        float zPositionPlayerSub = Player_Sub.transform.position.z;
+
+        if (zPositionPlayer > zPositionPlayerSub)
+        {
+            // Player가 Z축에서 더 높은 위치에 있으면 Player_Camera를 앞에 배치
+            SetCameraStackOrder(Player_Camera, Sub_Camera);
+        }
+        else
+        {
+            // Player_Sub가 Z축에서 더 높은 위치에 있으면 Sub_Camera를 앞에 배치
+            SetCameraStackOrder(Sub_Camera, Player_Camera);
+        }
+    }
+    private void SetCameraStackOrder(Camera frontCamera, Camera backCamera)
+    {
+        var baseCameraData = camera.GetUniversalAdditionalCameraData();
+        if (baseCameraData != null)
+        {
+            baseCameraData.cameraStack.Clear(); // 기존 스택 초기화
+            baseCameraData.cameraStack.Add(frontCamera);
+            baseCameraData.cameraStack.Add(backCamera);
+        }
+        else
+        {
+            Debug.LogError("Main camera does not have UniversalAdditionalCameraData.");
+        }
+    }
+}
