75553b1bd17ecc2e9ccaf4d608e58cb05ffb45b5
RyanSong <tjr6255@gmail.com>
2024-07-17 09:51:45 +0900

    [Commit] before merge.

--- Assets/Scripts/Managers/SubPlayerSkillManager.cs
+++ Assets/Scripts/Managers/SubPlayerSkillManager.cs
@@ -4,15 +4,169 @@ using UnityEngine;
 
 public class SubPlayerSkillManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Skill Object List")]
+    [SerializeField] private GameObject Wolfs;
+    //[SerializeField] private List<GameObject> bloomObjects = new List<GameObject>();
+    [SerializeField] private List<GameObject> afterEffect = new List<GameObject>();
+
+    [Header("Player variable")]
+    [SerializeField] private Animator p_Anim;
+    [SerializeField] public bool isCasting = false; // 애니메이션 실행중 재입력 불가
+
+    [Header("Hit Effect")]
+    [SerializeField] private BoxCollider SkillColi;
+    [SerializeField] private int targetLayer;
+    [SerializeField] private float hitEffectDalayTime; // 슬로우모션 지속 시간
+    [SerializeField] private float slowMotionDuration; // 슬로우모션 지속 시간
+    [SerializeField] private float slowMotionFactor; // 시간 느리게 가는 정도
+
+    [Header("Hit Effect Timing")]
+    [SerializeField] private GameObject hitEffect;
+    [SerializeField] private float hitLoopTimeLimit = 2.0f;
+
+    [Header("Hit Effect Spawn options")]
+    [SerializeField] private float hitSpawnScale = 1.0f;
+
+    [Header("Bite Effect Timing")]
+    [SerializeField] private GameObject biteTrailEffect;
+
+    [Header("Move Forward Timing")]
+    [SerializeField] private float totalDistance; // 이동할 총 거리
+    [SerializeField] private float moveForwardTime; // 이동 시간
+
     void Start()
     {
-        
+        // 타겟 레이어를 설정합니다. 예를 들어, 레이어 이름이 "Monster"라면:
+        targetLayer = LayerMask.NameToLayer("Monster");
+
+        // 임시적으로 물기 콜라이더 끄기.
+        SkillColi.enabled = false;
+
+        biteTrailEffect.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !isCasting)
+        {
+            OnAttack();
+        }
+    }
+
+    void OnAttack()
+    {
+        isCasting = true;  // 애니메이션 시작 시 공격 상태로 전환
+
+        // 공격 애니메이션 트리거 실행
+        p_Anim.SetTrigger("BasicAtk");
+
+        SkillColi.enabled = true;
+
+        //StartCoroutine(MoveForwardByDiameter());
+
+        StartCoroutine(SwordEffectLoop());
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //PlayerSkillManager psm = transform.GetComponent<PlayerSkillManager>();
+        // 충돌한 객체의 레이어가 타겟 레이어인지 확인합니다.
+        //if (other.gameObject.layer == targetLayer && !psm.isCasting)
+        if (other.gameObject.layer == targetLayer)
+        {
+            Debug.Log("Monster Collider와 충돌 감지!");
+
+            // 여기서 충돌 처리를 합니다.
+            MonsterView hitMonster = other.GetComponent<MonsterView>();
+            hitMonster.HurtDamage(150, transform);
+
+            StartCoroutine(HitEffectLoop(other));
+            StartCoroutine(SlowMotionEffect());
+        }
+    }
+
+    IEnumerator SwordEffectLoop()
+    {
+        biteTrailEffect.SetActive(true);
+
+        yield return new WaitForSeconds(0.1f);
+
+        SkillColi.enabled = false;
+
+        float elapsed = 0.0f;
+
+        Vector3 startPosition = transform.position;
+        Vector3 targetPosition = startPosition + transform.forward * totalDistance;
+
+        while (elapsed < moveForwardTime)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, elapsed / moveForwardTime);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        // 마지막 위치 보정
+        transform.position = targetPosition;
+
+        yield return new WaitForSeconds(hitLoopTimeLimit);
+
+        isCasting = false;  // 공격 애니메이션이 끝난 후 공격 상태 해제
+
+        biteTrailEffect.SetActive(false);
+    }
+
+    IEnumerator HitEffectLoop(Collider other)
+    {
+        yield return new WaitForSeconds(hitEffectDalayTime);
+
+        Vector3 hitPoint = other.ClosestPoint(transform.position); // 충돌 지점 추정
+        Vector3 newPosition = hitPoint + new Vector3(0, 0.1f, 0);
+
+        GameObject effectPlayer = (GameObject)Instantiate(hitEffect, newPosition, transform.rotation);
+
+        effectPlayer.transform.localScale = new Vector3(hitSpawnScale, hitSpawnScale, hitSpawnScale);
+
+        //if (disableLights && effectPlayer.GetComponent<Light>())
+        //{
+        //    effectPlayer.GetComponent<Light>().enabled = false;
+        //}
+        //
+        //if (disableSound && effectPlayer.GetComponent<AudioSource>())
+        //{
+        //    effectPlayer.GetComponent<AudioSource>().enabled = false;
+        //}
+
+        yield return new WaitForSeconds(hitLoopTimeLimit);
+
+        Destroy(effectPlayer);
+    }
+
+    private IEnumerator SlowMotionEffect()
+    {
+        Time.timeScale = slowMotionFactor;
+        yield return new WaitForSecondsRealtime(slowMotionDuration);
+        Time.timeScale = 1f;
+    }
+
+    // 여기에 transform rotation의 방향으로 transform position을 지름1만큼의 크기만큼 앞으로 변경하는 메서드 작성해줘
+    IEnumerator MoveForwardByDiameter()
+    {
+        //float totalDistance = 6.0f; // 이동할 총 거리
+        //float duration = 0.8f; // 이동하는 데 걸리는 시간
+        float elapsed = 0.0f;
+
+        Vector3 startPosition = transform.position;
+        Vector3 targetPosition = startPosition + transform.forward * totalDistance;
+
+        while (elapsed < moveForwardTime)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, elapsed / moveForwardTime);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        // 마지막 위치 보정
+        transform.position = targetPosition;
     }
 }
